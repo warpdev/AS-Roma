66f862f601ba2d944bd4b1a24cd220b70fd9dcf1
warpdev <98dngus@gmail.com>
2020-11-18 01:52:27 +0900

    Initial Commit

---
+++ AS Roma/AS_RomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AS_RomaApp.swift
+//  AS Roma
+//
+//  Created by 박우현 on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct AS_RomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
